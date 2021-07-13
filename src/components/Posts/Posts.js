bfc6217b119cbe7d2338c24358342ed17c827848
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-13 13:30:54 -0400

    Figuring out my mess of errors - need to slow down

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,20 +2,17 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
-const Posts = (props) => {
+export const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  export const Posts = (props) => {
-    const { likePost, posts } = props;
-    const { likePost, post } = props;
+  const { likePost, posts } = props;
 
-    return (
-      <div className="posts-container-wrapper">
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} likePost={likePost} />;
-        })}
-      </div>
-    );
-  };
+  return (
+    <div className="posts-container-wrapper">
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} likePost={likePost} />;
+      })}
+    </div>
+  );
 };
 
 export default Posts;
